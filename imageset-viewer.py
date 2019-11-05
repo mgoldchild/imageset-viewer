338ba44d5f9a87e2fedf77022df3644d4299bd8b
mgoldchild <mgoldchild@gmail.com>
2019-11-05 16:34:25 +0900

    tweak width of listbox

--- imageset-viewer.py
+++ imageset-viewer.py
@@ -14,19 +14,24 @@
 
 from __future__ import print_function
 
+import os
+
+from lxml import etree
+from PIL import Image, ImageTk  # pillow模块
+
+import cv2
+import numpy as np
+
 try:
     import Tkinter as tk
 except:
     import tkinter as tk
-from PIL import Image, ImageTk #pillow模块
-import os
+
 try:
     from tkFileDialog import askdirectory
 except:
     from tkinter.filedialog import askdirectory
-import cv2
-from lxml import etree
-import numpy as np
+
 
 class PascalVOC2007XML:
     def __init__(self, xml_full_name):
@@ -36,7 +41,7 @@ class PascalVOC2007XML:
 
     def get_boxes(self):
         if len(self.boxes) == 0:
-            for bbox in self.tree.xpath('//bndbox'):
+            for bbox in self.tree.xpath("//bndbox"):
                 pts = bbox.getchildren()
                 box = [int(float(_.text)) for _ in pts]
                 self.boxes.append(box)
@@ -58,57 +63,65 @@ class App:
         self.box_thick = box_thick
 
         self.im_dir = tk.StringVar()
-        self.path_entry = tk.Entry(master, text=self.im_dir, width=60, state='readonly')
+        self.path_entry = tk.Entry(master, text=self.im_dir, width=60, state="readonly")
         self.path_entry.grid(row=0, column=0)
 
-        self.choose_path_btn = tk.Button(master, text='输入路径', command=self.selectPath)
+        self.choose_path_btn = tk.Button(master, text="输入路径", command=self.selectPath)
         self.choose_path_btn.grid(row=0, column=1)
 
-        ## 设定封面
+        # 设定封面
         self.tkim = self.get_surface_tkim()
-        ## 也可以自己搞张图，替代默认的很丑的封面
-        #im_name = '/home/chris/Pictures/im/girl2.jpg'
-        #self.tkim = ImageTk.PhotoImage(Image.open(im_name))
+        # 也可以自己搞张图，替代默认的很丑的封面
+        # im_name = '/home/chris/Pictures/im/girl2.jpg'
+        # self.tkim = ImageTk.PhotoImage(Image.open(im_name))
 
-        self.label1 = tk.Label(master, justify='left',
-                          image=self.tkim, compound='center',
-                          fg='white', bg='white',
-                          width = 1000, height = 1000)
+        self.label1 = tk.Label(
+            master,
+            justify="left",
+            image=self.tkim,
+            compound="center",
+            fg="white",
+            bg="white",
+            width=1000,
+            height=1000,
+        )
         self.label1.grid(row=1, column=0)
 
         self.scrollbar = tk.Scrollbar(master, orient=tk.VERTICAL)
 
-        self.listbox = tk.Listbox(master, yscrollcommand=self.scrollbar.set)
-        self.listbox.grid(row=1, column=2, sticky=tk.N+tk.S)
+        self.listbox = tk.Listbox(master, yscrollcommand=self.scrollbar.set, width=100)
+        self.listbox.grid(row=1, column=2, sticky=tk.N + tk.S)
 
         self.im_names = []
         if im_dir is not None:
             self.im_dir.set(im_dir)
-            #获取自然顺序的文件列表
+            # 获取自然顺序的文件列表
             self.im_names = [_ for _ in os.listdir(self.im_dir.get())]
             self.im_names.sort()
             for im_name in self.im_names:
                 self.listbox.insert(tk.END, im_name)
-        self.listbox.bind('<<ListboxSelect>>', self.callback)
+        self.listbox.bind("<<ListboxSelect>>", self.callback)
         self.scrollbar.config(command=self.listbox.yview)
-        self.scrollbar.grid(row=1, column=3, sticky=tk.N+tk.S)
+        self.scrollbar.grid(row=1, column=3, sticky=tk.N + tk.S)
 
     def callback(self, event=None):
         im_id = self.listbox.curselection()
         if im_id:
             im_name = self.listbox.get(im_id)
-            if (im_name.endswith('.jpg') or im_name.endswith('.png')):
-                im_name_full = os.path.join(self.im_dir.get(), im_name).replace('\\', '/')
+            if im_name.endswith(".jpg") or im_name.endswith(".png"):
+                im_name_full = os.path.join(self.im_dir.get(), im_name).replace(
+                    "\\", "/"
+                )
                 self.tkim = self.get_tkim(im_name_full)
                 self.label1.configure(image=self.tkim)
-                #print(im_name_full)
+                # print(im_name_full)
 
     def get_tkim(self, im_name_full):
         """
         读取图像并转化为tkim。必要时做resize
         """
         im = cv2.imread(im_name_full)
-        print('reading image:', im_name_full)
+        print("reading image:", im_name_full)
         im_ht, im_wt, im_dt = im.shape
         show_x = self.show_x
         show_y = self.show_y
@@ -116,25 +129,34 @@ class App:
             show_x = im_wt
         if show_y is None:
             show_y = im_ht
-        if show_x!=im_wt or show_y!=im_ht:
+        if show_x != im_wt or show_y != im_ht:
             im = cv2.resize(im, (show_x, show_y))
-            print('doing resize!')
-            print('show_x={:d}, im_wt={:d}, show_y={:d}, im_ht={:d}'.format(show_x, im_wt, show_y, im_ht))
-        scale_x = im_wt*1.0 / show_x
-        scale_y = im_ht*1.0 / show_y
-        anno_name_full = im_name_full.replace('JPEGImages', 'Annotations').replace('.jpg', '.xml').replace('.png', '.xml')
-        print('anno_name_full is:', anno_name_full)
+            print("doing resize!")
+            print(
+                "show_x={:d}, im_wt={:d}, show_y={:d}, im_ht={:d}".format(
+                    show_x, im_wt, show_y, im_ht
+                )
+            )
+        scale_x = im_wt * 1.0 / show_x
+        scale_y = im_ht * 1.0 / show_y
+        anno_name_full = (
+            im_name_full.replace("JPEGImages", "Annotations")
+            .replace(".jpg", ".xml")
+            .replace(".png", ".xml")
+        )
+        print("anno_name_full is:", anno_name_full)
         if os.path.exists(anno_name_full):
-            print(' existing the xml file!')
+            print(" existing the xml file!")
             boxes = self.get_boxes_from_voc_xml(anno_name_full)
             for box in boxes:
-                cv2.rectangle(im,
-                          pt1=(int(box[0]/scale_x), int(box[1]/scale_y)),
-                          pt2=(int(box[2]/scale_x), int(box[3]/scale_y)),
-                          color=(0, 255, 0),
-                          thickness=self.box_thick
-                          )
-        im = im[:, :, ::-1]  #bgr => rgb   necessary
+                cv2.rectangle(
+                    im,
+                    pt1=(int(box[0] / scale_x), int(box[1] / scale_y)),
+                    pt2=(int(box[2] / scale_x), int(box[3] / scale_y)),
+                    color=(0, 255, 0),
+                    thickness=self.box_thick,
+                )
+        im = im[:, :, ::-1]  # bgr => rgb   necessary
         tkim = ImageTk.PhotoImage(Image.fromarray(im))
         return tkim
 
@@ -142,12 +164,14 @@ class App:
         """封面图片"""
         im = np.ndarray((500, 700, 3), dtype=np.uint8)
 
-        cv2.putText(im, 'Please choose image set folder',
-                    (30, 200),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    1,
-                    color=(255,255,255)
-                    )
+        cv2.putText(
+            im,
+            "Please choose image set folder",
+            (30, 200),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            color=(255, 255, 255),
+        )
         im = im[:, :, ::-1]
 
         tkim = ImageTk.PhotoImage(image=Image.fromarray(im))
@@ -162,8 +186,8 @@ class App:
     def selectPath(self):
         pth = askdirectory()
 
-        #清空listbox中的元素
-        self.listbox.delete(0, len(self.im_names)-1)
+        # 清空listbox中的元素
+        self.listbox.delete(0, len(self.im_names) - 1)
 
         self.fill_im_names(pth)
 
@@ -176,13 +200,14 @@ class App:
             for im_name in self.im_names:
                 self.listbox.insert(tk.END, im_name)
 
-# example usage:
-if __name__ == '__main__':
-    root = tk.Tk()  #创建窗口对象的背景色
-    root.title('imageset viewer')
-    root.geometry('1400x1400') #设置窗口大小
 
-    ## 最简单的方式：不预设im_dir，打开GUI后自行选择图片路径
+# example usage:
+if __name__ == "__main__":
+    root = tk.Tk()  # 创建窗口对象的背景色
+    root.title("imageset viewer")
+    root.geometry("1400x1400")  # 设置窗口大小
+
+    # 最简单的方式：不预设im_dir，打开GUI后自行选择图片路径
     app = App(root, im_dir=None, box_thick=2)
 
     """
@@ -200,5 +225,5 @@ if __name__ == '__main__':
     app = App(root, im_dir, show_x=1000, show_y=1000, box_thick=2)
     """
 
-    #进入消息循环
+    # 进入消息循环
     root.mainloop()
